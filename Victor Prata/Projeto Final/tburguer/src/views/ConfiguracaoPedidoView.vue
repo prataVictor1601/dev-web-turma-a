e2527195648a8b65ad18571690aaa9ff17ff362e
Victor Rezende de Melo Prata <victorprata@dinfor.uniceub>
2026-05-21 21:46:45 -0300

    atualizacao projeto final

--- Victor Prata/Projeto Final/tburguer/src/views/ConfiguracaoPedidoView.vue
+++ Victor Prata/Projeto Final/tburguer/src/views/ConfiguracaoPedidoView.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        <h1>Configuração do pedido</h1>
-    </div>
+  <div>
+    <h1>Configuração do Pedido</h1>
+    <pedido-component-vue :burguer="this.hamburguerSelecionado" />
+  </div>
 </template>
 <script>
-    export default{
-        name: 'ConfiguraçãoPedidosView'
+import PedidoComponentVue from "@/components/PedidoComponent.vue";
+
+export default {
+  name: "ConfiguracaoPedidoView",
+  components: {
+    PedidoComponentVue,
+  },
+  data() {
+    return {
+      hamburguerSelecionado: null,
+    };
+  },
+  mounted() {
+    const query = this.$route.query;
+    if (query.burguer) {
+      const decodeBurguer = JSON.parse(decodeURIComponent(query.burguer));
+      this.hamburguerSelecionado = decodeBurguer;
     }
+  },
+};
 </script>
 <style scoped></style>
