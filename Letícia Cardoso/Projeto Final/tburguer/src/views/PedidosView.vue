fb23ba3427da963c71958f70046887c139d4ad8e
Letícia Vitória Cardoso Cunha <letticardoso@dinfor.uniceub>
2026-05-28 21:56:35 -0300

    avanço do projeto final

--- Letícia Cardoso/Projeto Final/tburguer/src/views/PedidosView.vue
+++ Letícia Cardoso/Projeto Final/tburguer/src/views/PedidosView.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <h1>Pedidos</h1>
+        <lista-pedido-component/>
     </div>
 </template>
 <script>
+import ListaPedidoComponent from '@/components/ListaPedidoComponent.vue';
     export default {
         name: 'PedidosView',
+        components: {
+            ListaPedidoComponent
+        }
     };
 </script>
 <style scoped></style>
